f78d7e9b39b4ab95f47197bcb2e463b0ba26bef9
FarzanaBony <114426314+FarzanaBony@users.noreply.github.com>
2022-10-20 02:59:21 -0400

    Setting_Interface2

--- src/main/java/com/mycompany/texaspoker/TexasPoker.java
+++ src/main/java/com/mycompany/texaspoker/TexasPoker.java
@@ -4,6 +4,9 @@
  */
 
 package com.mycompany.texaspoker;
+import java.awt.Color;
+//import java.awt.Dimension;
+//import javax.swing.JFrame;
 
 /**
  *
@@ -13,5 +16,42 @@ public class TexasPoker {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+           
+     
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Settings_Interface2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Settings_Interface2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Settings_Interface2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Settings_Interface2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Settings_Interface2 frame = new Settings_Interface2();
+                //Color color = new Color(180, 192 ,250);
+                Color color = new Color(84, 151 ,220);
+                frame.getContentPane().setBackground(color);
+                frame.setVisible(true);
+            }
+        });
+
     }
 }
