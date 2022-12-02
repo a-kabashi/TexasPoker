c47e4cd385566e204aa14618460f9db15085b26f
FarzanaBony <114426314+FarzanaBony@users.noreply.github.com>
2022-12-02 12:58:46 -0500

    added

---
+++ src/main/java/com/mycompany/texaspoker/Dealer.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.texaspoker;
+
+/**
+ *
+ * @author tasni
+ */
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Dealer {
+
+private ArrayList<Integer> deck;   
+ private int[] communityCards; 
+ private int[] player1Cards;
+ private int[] player2Cards;
+ 
+  Dealer()
+  {
+    this.communityCards = new int[5];
+    this.player1Cards = new int[2];
+    this.player2Cards = new int[2];
+    this.deck = new ArrayList<Integer>(51);
+  }
+    
+  public int getOneCard(int[] cardNumber) 
+ {  
+     int[] card = cardNumber;
+     int oneCard = card[new Random().nextInt(card.length)];
+     return oneCard;
+ } 
+ 
+ public int[] getCommunityCards()
+{ 
+    return this.communityCards;
+}
+ 
+public int[] getPlayer1TowCards()
+{
+ return this.player1Cards; 
+}
+
+public int[] getPlayer2TowCards()
+{
+ return this.player2Cards; 
+}
+
+}
